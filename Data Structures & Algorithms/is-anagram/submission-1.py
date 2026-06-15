28dfba4fedd611e1cc77ab816f661e4e8eb3a6b3
natsu <161167704+narminexe@users.noreply.github.com>
2026-06-15 02:25:46 +0100

    Bulk sync: 11 submissions

--- Data Structures & Algorithms/is-anagram/submission-1.py
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -1,13 +1,9 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        Counter(s) == Counter(t)
-        if len(s) != len(t):
+        s = sorted(s)
+        t = sorted(t)
+        if s == t:
+            return True
+        else:
             return False
-        countS,countT = {},{}
-        for i in range(len(s)):
-            countS[s[i]] = 1+ countS.get(s[i],0)
-            countT[t[i]] = 1+ countT.get(t[i],0)
-        for c in countS:
-            if countS[c] != countT.get(c,0):
-                return False
-        return True    
+        
